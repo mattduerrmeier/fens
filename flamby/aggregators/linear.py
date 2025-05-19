Fix weighted average aggregator

flamby/aggregators/linear.py
@@ -1,7 +1,5 @@
 import logging
-
 import torch
-
 import wandb
 
 
@@ -25,7 +23,7 @@ def run_forward_linearagg(
     with torch.no_grad():
         for out, data in testset:
             out = (out.permute(*torch.arange(out.ndim - 1, -1, -1)) @ weights) + bias
-            out = out.permute(*torch.arange(outx.ndim - 1, -1, -1))
+            out = out.permute(*torch.arange(out.ndim - 1, -1, -1))
 
             loss = criterion(out, data)
 
@@ -55,8 +53,8 @@ def linear_mapping(
     wandb.define_metric(f"{id_str}/*", step_metric=f"{id_str}/epoch")
 
     weights = torch.ones(total_clients, requires_grad=True, dtype=torch.float32)
-    logging.info("Size of weights: {}".format(weights.size()))
     bias = torch.tensor([1.0], requires_grad=True, dtype=torch.float32)
+    logging.info("Size of weights: {}".format(weights.size()))
     logging.info("Size of bias: {}".format(bias.size()))
 
     lr = agg_params["lm_lr"]
@@ -78,7 +76,7 @@ def linear_mapping(
         for out, data in train_dataset:
             optimizer.zero_grad()
             out = (out.permute(*torch.arange(out.ndim - 1, -1, -1)) @ weights) + bias
-            out = out.permute(*torch.arange(outx.ndim - 1, -1, -1))
+            out = out.permute(*torch.arange(out.ndim - 1, -1, -1))
 
             # target = y.reshape(y_pred.shape) if not require_argmax else y
             loss = criterion(out, data)

flamby/aggregators/weighted_average.py
@@ -6,34 +6,21 @@ def weighted_averaging(
     dataset, metric, total_clients, num_classes, label_dists, require_argmax=False
 ):
     # Get weights from labels
-    my_labels_tensor = torch.tensor(label_dists)  # (num_clients, num_classes)
-    label_sum_tensor = my_labels_tensor.sum(dim=0)  # (num_classes)
-    my_weights_tensor = (
-        my_labels_tensor / label_sum_tensor
-    )  # (num_clients, num_classes)
+    labels_tensor = torch.tensor(label_dists)
+    label_sum_tensor = labels_tensor.sum(dim=0)
+    weighted_label_tensor = labels_tensor / label_sum_tensor
 
     inputs = []
     outputs = []
     for out, data in dataset:
-        local_weight = my_weights_tensor[:, labels.int()]
-        out = out * local_weight
+        if num_classes > 2:
+            labels = out[:, :, -num_classes:].argmax(dim=-1)
+        else:
+            labels = out[:, :, -1]
+
+        local_weight = torch.take_along_dim(weighted_label_tensor, labels.long(), dim=1)
+        out = out * local_weight.unsqueeze(dim=-1)
         out = torch.sum(out, dim=0)
-        # if require_argmax:
-        #     # out: (batch_size, total_clients * num_classes)
-        #     out = torch.reshape(out, (-1, total_clients, num_classes))
-        #     out = out * my_weights_tensor
-        #     y_pred = torch.sum(out, dim=1).argmax(dim=1)
-        # else:
-        #     # out: (batch_size, total_clients)
-        #     out = torch.sigmoid(out)
-        #     out_complement = 1 - out
-        #     out_all = torch.stack(
-        #         (out_complement, out), dim=2
-        #     )  # (batch_size, total_clients, 2)
-        #     out = out_all * my_weights_tensor  # (batch_size, total_clients, 2)
-        #     out = torch.sum(out, dim=1)
-        #     out = torch.softmax(out, dim=1)
-        #     out = torch.log(out[:, 1] / out[:, 0])
 
         inputs.append(data)
         outputs.append(out)

flamby/main.py
@@ -175,6 +175,7 @@ def run(args, device):
 
     trained_models: list[Autoencoder] = []
     proxy_datasets: list[torch.utils.data.Dataset] = []
+    labels_dists: list[list[int]] = []
 
     if not os.path.exists(args.result_dir):
         os.makedirs(args.result_dir)
@@ -205,6 +206,7 @@ def run(args, device):
             collate_fn=params["collate_fn"],
         )
         proxy_datasets.append(proxy_dataset)
+        labels_dists.append(client_label_distribution)
 
         torch.manual_seed(args.seed + client_idx)
 
@@ -265,7 +267,7 @@ def run(args, device):
                     "Skip visualization of client model as dataset is not visual"
                 )
 
-            print(f"Evaluating VAE {client_idx + 1} on downstream task:")
+            print(f"Evaluating VAE {client_idx + 1} on downstream task")
             evaluate_downstream_task(
                 id_str,
                 torch.utils.data.DataLoader(
@@ -348,6 +350,7 @@ def run(args, device):
         proxy_dataloader,
         test_dataloader,
         trained_models,
+        labels_dists,
         num_labels,
         mse_metric,
         device,